88c8ffacbb720d56af4c1e96899840912d4037e0
Craig Rasmussen <rasmus@uoregon.edu>
2014-04-18 12:26:09 -0700

    Fixed up to look more like is required for class.

--- week_3/minimum.py
+++ week_3/minimum.py
@@ -23,18 +23,48 @@ for line in infile:
    d = storms.line_to_dict(line)
    storm_list.append(d)
 
+## break into decades
+#
+for i in range(0,len(storm_list)):
+   d = storm_list[i]
+   d['year'] = (d['year']/10)*10
+   storm_list[i] = d
+
+'''
+Container data structure can be sorted:
+
 sorted_list = sorted(storm_list, key=lambda x: x['pressure'])
+'''
 
-name = ''
-names = []
+year = 1900
+decade  = []
+decades = []
 
-for d in sorted_list:
-   if name != d['name']:
-      name = d['name']
+count = 0
+minimum = 99999999   # a big number
+
+for d in storm_list:
+   if year == d['year']:
+      decade.append(d)
+      count += 1
+      if minimum > d['pressure']: minimum = d['pressure']
+   else:
+      print 'year:', year, ' minimum:', minimum, ' number:', count, ' average:', count/10.
+
+      year += 10
+      count = 0
+      minimum = 99999999
+
+# print end decade values
+print 'year:', year, ' minimum:', minimum, ' number:', count, ' average:', count/10.
+
+'''
+      year = d['year']
       if names.__contains__(name) == False:
          l = storms.dict_to_list(d)
          outfile.write(str(l) + ',' + '\n')
       names.append(name)
+'''
 
 ## close all open files
 #
